4c16bbf53cdc2fccfe4c2255cc2034c5e25db067
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-05-09 02:40:52 +0530

    DAY6

--- DAY6/script.js
+++ DAY6/script.js
@@ -12,3 +12,39 @@
 // PASCAL CASE
 // /FullName
 
+// bet = "Mridul";
+// console.log(bet)
+
+// let fullName = "Mridul Krishna";
+// let age = 25;
+// let totalPrice = 1000;
+
+// console.log(fullName);
+
+// var age = 24;
+// var age = 33;
+// var age = 34;
+// console.log(age)
+// var me leke kabhi nhi karna h jab bhi karenge toh most likely hum let or const me karenge
+
+// let age = 23;
+// age = 33
+// console.log(age)
+
+// {
+//     let a = 5;
+//     console.log(a);
+// }
+// {
+//     let a = 10;
+//     console.log(a)
+// }
+
+// let x;
+const student = {
+    fullName: "Mridul Krishna",
+    age: 13,
+    cgpa: 8.2,
+    isPass: true,
+};
+console.log(student)
