6da0d03d45bc068578ea42b6803a2caca983731c
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-05-09 18:50:28 +0530

    Operators&Conditionals

---
+++ DAY6/Operators&Conditionals/script.js
@@ -0,0 +1,20 @@
+// console.log("Hello World")
+// This is a comment
+/* this
+is
+a
+Multi
+Line
+Comment*/
+
+// ARITHEMATIC OPERATORS
+let a = 5;
+let b = 2;
+
+// console.log("a+b")
+// console.log(a+b)
+console.log("a+b=", a + b)
+console.log("a-b=", a - b)
+console.log("a*b=", a * b)
+console.log("a/b=", a / b)
+console.log("a%b=", a % b)
