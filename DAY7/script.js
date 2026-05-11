b2d9dac8a33124503d484b5d3d9dc1753ea5cc48
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-05-11 15:40:42 +0530

    DAY7

---
+++ DAY7/script.js
@@ -0,0 +1,109 @@
+// ARTHEMATIC OPERATORS
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a=", a, "& b=", b)
+// console.log("a+b=", a + b)
+// console.log("a-b=", a - b)
+// console.log("a*b=", a * b)
+// console.log("a/b=", a / b)
+// console.log("a%b=", a % b)
+// console.log("a**b=", a ** b)
+// console.log("a+b=", a + b)
+
+// UNARY OPERATORS
+// let a = 5;
+// let b = 2;
+
+// console.log("a=", a, "& b=", b)
+// a = a + 1;  //6
+// a++; //6
+// console.log(a);
+// b--;
+// console.log(b);
+// console.log("++a=", ++a);           //++a = 6
+// console.log("a++=", a++);            //a++ = 5      ,       a++ = 6
+
+// aose hi pre or post decrement kaam karta h
+
+// ASSIGNMENT OPERATORS
+// let a = 5;
+// let b = 2;
+
+// console.log("a=", a, "& b=", b);
+// a += 4; // a = a+4
+// console.log("a = ", a);
+
+// a -= 4; // a = a-4
+// console.log("a = ", a);
+
+// a *= 4; // a = a*4
+// console.log("a = ", a);
+
+// a /= 4; // a = a/4
+// console.log("a = ", a);
+
+// a %= 4; // a = a%4
+// console.log("a = ", a);
+
+// a **= 4; // a = a**4
+// console.log("a = ", a);
+
+
+// COMPARISION OPERATORS
+
+// let a = 5;
+// let b = 3;
+// // console.log(a==b);
+// // console.log(a!=b);
+// // console.log(a===b);
+// // console.log(a!==b);
+// console.log(a>b);
+// console.log(a>=b);
+// console.log(a<b);
+// console.log(a<=b);
+
+
+// LOGICAL OPERATORS
+// let a = 6;
+// let b = 5;
+
+// // let cond1 = a > b;      //true
+// // let cond2 = a === 6;      //true
+// // console.log(cond1 && cond2);
+
+// // let cond1 = a > b;      //true
+// // let cond2 = a === 6;      //true
+// // console.log(cond1 || cond2);
+
+// let cond1 = a > b;      //true
+// let cond2 = a === 6;      //true
+// console.log(!(cond1 < cond2));
+
+
+
+// CONDITIONAL STATEMENTS
+
+// let age = 16;
+// if (age >= 18) {
+//     console.log("can vote !")
+// }
+// if (age < 18) {
+//     console.log("cannot vote !")
+// }
+
+
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark")    {
+//     color = "black";
+// }
+// if (mode === "light"){
+//     color = "white";
+// }
+// console.log(color); 
+
+
+
