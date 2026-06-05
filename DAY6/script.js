ab9140822427af2a1f10be03054d866caadfa9ff
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-05 13:02:20 +0530

    DAY6

--- DAY6/script.js
+++ DAY6/script.js
@@ -1,10 +1,17 @@
-console.log("Mridul Krishna")
-fullName = "kallu badmos";
-age = 23;
-price = 99.99;
-a = null;
-b = undefined;
-isFollow = true;
+// console.log("Mridul Krishna")
+// fullName = "kallu badmos";
+// age = 23;
+// price = 99.99;
+// a = null;
+// b = undefined;
+// isFollow = true;
 
-fullName = 23;
+// fullName = 23;
+// console.log(fullName)
+
+let fullName = "Tony Stark";
+let age = 24;
+let totalPrice = 1000;
 console.log(fullName)
+let a;
+console.log(a)
