65f1551faecc25c5b48b5c5c1ef2a155a638d2c1
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-06 12:54:19 +0530

    DAY7

--- DAY7/script.js
+++ DAY7/script.js
@@ -12,10 +12,32 @@
 // }
 
 // Q2
-let gameNum = 25;
-let user = prompt("Guess the number:")
-while (user != gameNum) {
-    user = prompt(" you entered wrong number Guess the number again:")
+// let gameNum = 25;
+// let user = prompt("Guess the number:")
+// while (user != gameNum) {
+//     user = prompt(" you entered wrong number Guess the number again:")
 
-}
-console.log("you find the number ")
+// }
+// console.log("you find the number ")
+
+
+// let str = "Mridul Krishna";
+// len = str.length;
+// console.log(len);
+// console.log(str[3])
+
+
+
+// template literals
+// let sentence = `this is a template liiterals `
+
+// let str = "     Mridul Krishna  ";
+// i = str.trim();
+// console.log(i)
+
+// let str1 = "Mridul";
+// let str2 = " Krishna";
+// console.log(str1.concat(str2))
+
+let str = "Hello";
+console.log(str.replace("H","L"))
