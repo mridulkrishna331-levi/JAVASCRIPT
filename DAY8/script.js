77ad783a70467bc21955115f53512298e8e97d4e
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-07 12:40:17 +0530

    DAY8

---
+++ DAY8/script.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    console.log("wlecome to maanu ka ghar ")
+    console.log("ghar me abhi kaam chal rha h ")
+}
+myFunction()
+
+function heLLo(pudina) {
+    console.log(pudina)
+}
+heLLo("mengha h");
+
+
+
+// adding two number
+function sum(a, b) {
+    console.log(a + b)
+}
+
+sum(5655, 6868);
+
+function s(x, y) {
+    a = x + y;
+    return a;
+}
+
+let val = s(4, 5);
+console.log(val);
