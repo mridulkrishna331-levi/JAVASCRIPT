853eda9aadcd70bee1d92909491c2e7db0856505
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-08 12:12:04 +0530

    DAY9

--- DAY9/script.js
+++ DAY9/script.js
@@ -1,4 +1,8 @@
 let nums = [2, 4, 6, 7];
-nums.forEach((nums) => {
-    console.log(nums * nums)
+// nums.forEach((nums) => {
+//     console.log(nums * nums)
+// });
+
+nums.map((val) => {
+    console.log(val)
 });
