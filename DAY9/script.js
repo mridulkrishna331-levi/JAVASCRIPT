89c8e5e90a8dec13168cec71d49761fdc736a5f8
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-08 12:27:31 +0530

    DAy9

--- DAY9/script.js
+++ DAY9/script.js
@@ -3,6 +3,18 @@ let nums = [2, 4, 6, 7];
 //     console.log(nums * nums)
 // });
 
-nums.map((val) => {
-    console.log(val)
+let newArr = nums.map((val) => {
+    return val;
 });
+console.log(newArr)
+
+let newarr = nums.filter((val) => {
+    return val % 2 === 0;
+});
+console.log(newarr);
+
+
+let arr = [1, 2, 3, 4];
+arr.reduce((res, current) => {
+    return res + current
+})
