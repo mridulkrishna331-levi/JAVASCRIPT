a0bf1ef225b1e4571168248eb1b9791b7c3d1df3
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-28 09:52:03 +0530

    DAY12

--- DAY12/script.js
+++ DAY12/script.js
@@ -1,19 +1,31 @@
-console.log("This is a hello from the external JS file")
+// console.log("This is a hello from the external JS file")
 
 
-// VARIABLES
-var age = 22;
-var num1 = 23;
-var num2 = 46;
-console.log(num1 + num2);
-console.log(age);
+// // VARIABLES
+// var age = 22;
+// var num1 = 23;
+// var num2 = 46;
+// console.log(num1 + num2);
+// console.log(age);
 
 
-// if(true){
-//    var tinda = "hello"
-// }
-// console.log(tinda)
-console.log(bhindi)
-var bhindi = 23;
+// // if(true){
+// //    var tinda = "hello"
+// // }
+// // console.log(tinda)
+// console.log(bhindi)
+// var bhindi = 23;
+
+
+
+
+
+let age = 22;
+console.log(age)
+if (true) {
+    let bist = "hello there !"
+    console.log(bist)
+}
+// console.log(bist)     it will show error
 
 
