3603779f2312be068bc57468b30316a3ce05c0bb
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-28 11:41:11 +0530

    DAY12

--- DAY12/script.js
+++ DAY12/script.js
@@ -28,9 +28,53 @@
 // }
 // // console.log(bist)     it will show error
 
-const age = 23;
-// age = "piyush"       it willl show error that assignment to the constant variable
-console.log(age)
+// const age = 23;
+// // age = "piyush"       it willl show error that assignment to the constant variable
+// console.log(age)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DATATYPES
+// numbers
+let num = 2.12;
+let num1 = 10;
+console.log(num + num1)
+
+// strings
+const name = "manu";
+const next = "tiwari"
+console.log(name + ' ' + next);
+
+// boolean
+let isloggedin = true;
+console.log(isloggedin)
+
+// null 
+let lastLoginDate = null;
+console.log(lastLoginDate)
+
+// undefined
+let loginDate = undefined;
+console.log(loginDate);
+
+// objects
+const person = {
+    FullName:"Mridul Krishna",
+    age : 23,
+    isloggedin:false,
+}
+console.log(person )
 
