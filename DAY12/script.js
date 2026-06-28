c6a86d7c5117e4e8bf1185284706854dcb39a135
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-28 09:47:10 +0530

    DAY12

--- DAY12/script.js
+++ DAY12/script.js
@@ -1,1 +1,19 @@
 console.log("This is a hello from the external JS file")
+
+
+// VARIABLES
+var age = 22;
+var num1 = 23;
+var num2 = 46;
+console.log(num1 + num2);
+console.log(age);
+
+
+// if(true){
+//    var tinda = "hello"
+// }
+// console.log(tinda)
+console.log(bhindi)
+var bhindi = 23;
+
+
