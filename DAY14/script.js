40a19af63913d27e2f59d3fef6478c1ba5344338
mridulkrishna331-levi <mridulkrishna331@gmail.com>
2026-06-30 09:44:48 +0530

    DAY13

---
+++ DAY14/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("btn");
+const username = document.getElementById("name");
+button.addEventListener("click", () => {
+    const value = username.value;
+    localStorage.setItem("name", value);
+});
