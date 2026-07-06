ce97794a622128ab7e20a00a41e4ee304cea3ea5
MRIDUL  KRISHNA <mridulkrishna331@gmail.com>
2026-07-06 23:09:23 +0530

    Update script.js

--- DAY14/script.js
+++ DAY14/script.js
@@ -1,6 +1,7 @@
 const button = document.getElementById("btn");
 
 const username = document.getElementById("name");
+
 button.addEventListener("click", () => {
     const value = username.value;
     localStorage.setItem("name", value);
